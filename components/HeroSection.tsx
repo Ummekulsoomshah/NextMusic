3caa371eb1725c1b2df0c872ab1a941b38bfd49c
Syeda Umme Kulsoom <125365023+Ummekulsoomshah@users.noreply.github.com>
2024-05-13 07:11:54 +0500

    Update HeroSection.tsx

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -13,14 +13,14 @@ function HeroSection() {
             >Master the art of music</h1>
             <p
                 className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto"
-            >Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.</p>
+            >Dive into our comprehensive music courses and transform your musical journey today. Whether you are a beginner or looking to refine your skills, join us to unlock your true potential.</p>
             <div className="mt-4">
                 <Link href={"/courses"}>
                 <Button
         borderRadius="1.75rem"
         className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
       >
-       Let's feel it
+       Just feel it
       </Button>
                 </Link>
             </div>
